9730bc5e0127aca04f5089e71cd7f5cffa718968
unknown <cbenshof@mail.umw.edu>
2013-04-10 11:25:30 -0400

    action listener and image added

--- logIn.java
+++ logIn.java
@@ -1,56 +1,88 @@
-import java.awt.*;
-
+import java.awt.event.*;
 import javax.swing.*;
 
 
-public class logIn extends JFrame{
-	   
+public class logIn extends JFrame implements ActionListener{
+	JLabel username, password, imageLabel;
+	JTextArea uName, pWord, temp;
+	JButton submit, register;
+	ImageIcon image;
+	
 	//constructor
 	public logIn(){
 		super("SPYtter");
+		pack();
+		setVisible(true);
 		setSize(1200,800);
 		GUI();	
 	    }
 	
 	private void GUI() {
-		
 
-		// gui components
+		//gui components
 		JFrame frame = new JFrame();
 		JPanel pane = new JPanel();
-		
-		// buttons and areas
-		JTextArea uName = new JTextArea("Spy Alias");
-		JTextArea pWord = new JTextArea("Top Secret Password");
-		JButton submit = new JButton("Login");
-		JButton register = new JButton("Register");
-		
-		// sizing GUI components
-		submit.setBounds(200,200,100,50);
-		register.setBounds(200,200,100,50);
-		uName.setBounds(200,200,100,50);
-		pWord.setBounds(200,200,100,50);
-		
-		// adding action listeners
-	
 		
 		// sizing, making visible, and adding pane to Frame
 		frame.setLayout(null);
 		frame.setVisible(true);
 		frame.setSize(1600,850);
 		frame.add(pane);
-	
 		
-		// adding components to panel and sizing
-		pane.setLayout(null);
-		pane.setSize(1600,850);
+		//add image 
+		image = new ImageIcon("C:\\eclipse\\330twitter\\src\\SPYder.jpg");
+		imageLabel = new JLabel(image,JLabel.CENTER);
+		pane.add(imageLabel);
+		
+		// buttons and areas
+		username = new JLabel("Spy Alias: ");
+		password = new JLabel("Top Secret Password: ");
+		uName = new JTextArea();
+		pWord = new JTextArea();
+		temp =  new JTextArea();
+		submit = new JButton("Login");
+		register = new JButton("Register");
+		
+		// sizing GUI components
+		username.setBounds(400,200,150,25);
+		password.setBounds(400,300,150,25);
+		uName.setBounds(600,200,100,25);
+		pWord.setBounds(600,300,100,25);
+		temp.setBounds(400,500,300,50);
+		submit.setBounds(400,400,100,25);
+		register.setBounds(600,400,100,25);
+
+	
+		// adding action listeners
+		submit.addActionListener(this);
+		register.addActionListener(this);
+		
+		// adding components to panel
+		pane.add(username);
+		pane.add(password);
 		pane.add(uName);
 		pane.add(pWord);
+		pane.add(temp);
 		pane.add(submit);
 		pane.add(register);
+		pane.setLayout(null);
+		pane.setSize(1600,850);
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 	}
-
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand().equals("Login")){
+			String userName = uName.getText();
+			String passWord = pWord.getText();
+			temp.setText("You pressed login" + "\nUsername: "+ userName+ "\nPassword: "+passWord);
+		}
+		else if (e.getActionCommand().equals("Register")){
+			temp.setText("You pressed register");
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 			logIn log = new logIn();
 	}
